b2d4fbb9fc314007bc2ae144666d13e2a9974744
fwaters77 <33361448+fwaters77@users.noreply.github.com>
2018-02-05 13:02:41 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,4 +6,22 @@ import csv
 csvpath =os.path.join("PyPoll", "election_data_1.csv")
 csvpath =os.path.join("PyPoll", "election_data_2.csv")
 
+#make list to store votes
+total_votes = []
+candidate_list = []
+vote_percentage = []
+candidate_votes = []
+winner = []
+
+#Open CSV
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Loop through rows
+    #The total number of votes cast
+    #vote counter
+    voterID = 0
+    for row in csvreader:
+
+        voterID += 1
 
